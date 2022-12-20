aadc8bab90b52b8279e3b381c204d5a34d6803e9
szwszw <357243507@qq.com>
2022-12-20 14:41:22 +0800

    first commit3

--- src/main/java/com/szw/Demo02.java
+++ src/main/java/com/szw/Demo02.java
@@ -11,5 +11,6 @@ public class Demo02 {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello333");
     }
 }
