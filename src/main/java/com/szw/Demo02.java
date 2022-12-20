ac0cfbaeb6280518379900a768e9980d0054f7a2
szwShen <51107770+szwShen@users.noreply.github.com>
2022-12-20 16:09:11 +0800

    change 3333

--- src/main/java/com/szw/Demo02.java
+++ src/main/java/com/szw/Demo02.java
@@ -12,6 +12,6 @@ public class Demo02 {
         System.out.println("hello");
         System.out.println("hello2222");
         System.out.println("hello");
-        System.out.println("hello333");
+    
     }
 }
